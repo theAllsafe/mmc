220706c24195ec18473c2ffcdaa5b5b84a3921b8
Devops <98335787+theAllsafe-Devops@users.noreply.github.com>
2022-04-19 16:29:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Router basename={"/mmc/build"}>
+        <Router>
           <BaseRoute />
         </Router>
       </div>
